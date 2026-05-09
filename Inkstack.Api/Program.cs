0fca95451ba1cabda7ed0c9ad062dd1dc360be39
André Lashley <andre.lashley@gmail.com>
2026-05-09 14:16:26 -0700

    Initial setup

---
+++ Inkstack.Api/Program.cs
@@ -0,0 +1,56 @@
+using Inkstack.Core.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+
+var posts = new List<Post> 
+{
+    new Post { Id = 1,  Title = "Hello World", ShortDescription = "Hello World!" },
+    new Post { Id = 2,  Title = "Hello World", ShortDescription = "Hello World!" },
+    new Post { Id = 3,  Title = "Hello World", ShortDescription = "Hello World!" },
+};
+
+app.MapGet("/posts", () => 
+{ 
+    return posts;
+});
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ Inkstack.Core/Models/Post.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inkstack.Core.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string ShortDescription { get; set; } = string.Empty;
+    }
+}
+
+
+/*
+ * 
+ * 
+ * public class Post
+    {
+	    public virtual string Description
+	    { get; set; }
+
+	    public virtual string Meta
+	    { get; set; }
+
+	    public virtual string UrlSlug
+        { get; set; }
+
+	    public virtual bool Published
+	    { get; set; }
+
+	    public virtual DateTime PostedOn
+	    { get; set; }
+
+	    public virtual DateTime? Modified
+	    { get; set; }
+
+	    public virtual Category Category
+	    { get; set; }
+
+	    public virtual IList<Tag> Tags
+	    { get; set; }
+    }
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
